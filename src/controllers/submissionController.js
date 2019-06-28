769a616d336acf6f6670027132de5804b7facafe
lawrence <lawrence.t.yen@gmail.com>
2019-06-28 14:39:27 -0700

    added mailgun

--- src/controllers/submissionController.js
+++ src/controllers/submissionController.js
@@ -1,5 +1,7 @@
 const submissionQueries = require('../db/queries.submission.js');
 const mongoose = require('mongoose');
+const Mailgun = require('mailgun').Mailgun;
+const mg = new Mailgun(process.env.mgApi); //Need Mailgun API key
 
 module.exports = {
   index(req, res, next) {
@@ -37,6 +39,17 @@ module.exports = {
         data: submission
       };
       res.json(returnData)
+      mg.sendText('sender@example.com',
+        ['recipient@example.com'],
+        'Email Subject',
+        `Email message, ${req.body.name}, ${req.body.phone}, ${req.body.email} etc`,
+        'Origin server (if needed)',
+        (err) => {
+          if (err) {
+            console.log('Mailgun error:', err)
+          }
+        }
+      )
     })
     .catch((err) => {
       console.log(err)
