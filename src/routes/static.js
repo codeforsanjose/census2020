a9c751228177ddabcfd2dd41a67064ce0ef629bb
lawrence <lawrence.t.yen@gmail.com>
2019-06-30 23:02:28 -0700

    removed static route

--- src/routes/static.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Census 2020");
-});
-
-module.exports = router;
