43c5da5adb7dc30287a9e43b13ac30775d17148c
Turner Hayes <dev@turnerhayes.com>
2019-07-25 19:28:30 -0700

    i18n the navigation links

--- client/components/Navigation.js
+++ client/components/Navigation.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import './Navigation.scss';
 import { LocalePicker } from './LocalePicker';
+import { FormattedMessage } from 'react-intl';
 
 export default class Navigation extends Component {
   render () {
@@ -11,8 +12,20 @@ export default class Navigation extends Component {
         <LocalePicker
         />
         <div className="c_navigation__links">
-          <Link to="/">Home</Link>
-          <Link to="/contact">Contact</Link>
+          <Link to="/">
+            <FormattedMessage
+              id="navigation.links.home"
+              defaultMessage="Home"
+              description="'Home' link in the navigation bar"
+            />
+          </Link>
+          <Link to="/contact">
+            <FormattedMessage
+              id="navigation.links.contact"
+              defaultMessage="Contact"
+              description="'Contact' link in the navigation bar"
+            />
+          </Link>
         </div>
       </nav>
     );
