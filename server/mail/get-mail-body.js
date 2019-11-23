01191de8c3afda9d0595defa386868897eb29afa
Turner Hayes <dev@turnerhayes.com>
2019-11-22 22:15:58 -0800

    Add component for inquiry email

--- server/mail/get-mail-body.js
+++ server/mail/get-mail-body.js
@@ -1,6 +1,23 @@
 const { createElement } = require('react');
 const { renderToStaticMarkup } = require('react-dom/server');
 const { ConfirmationEmail, ConfirmationEmailSubject } = require('./mail-components/compiled/ConfirmationEmail');
+const { InquiryEmail } = require('./mail-components/compiled/InquiryEmail');
+
+module.exports.getInquiryMessage = ({ language, firstName, lastName, email, interests, comment }) => {
+  return renderToStaticMarkup(
+    createElement(
+      InquiryEmail,
+      {
+        language,
+        firstName,
+        lastName,
+        email,
+        interests,
+        comment
+      }
+    )
+  );
+};
 
 module.exports.getConfirmationMessage = ({ language, name, interests, comment }) => {
   return renderToStaticMarkup(
---
+++ server/mail/mail-components/InquiryEmail.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const path = require('path');
+const i18nDir = path.join(__dirname, '../../../../i18n');
+const {
+  supportedLocales,
+  supportedLocaleEnglishNames
+} = require(path.join(i18nDir, '/supported-locales'));
+
+/**
+ * Formats the date into a human-readable string
+ *
+ * @param {Date} date the date to format
+ *
+ * @return {string} the formatted date
+ */
+const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let amPm = 'AM';
+
+  if (hours >= 12) {
+    amPm = 'PM';
+    if (hours > 12) {
+      hours = hours % 12;
+    }
+  }
+
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+
+  return `${month}/${day}/${year} ${hours}:${minutes}${amPm}`;
+};
+
+export const InquiryEmail = ({
+  firstName,
+  lastName,
+  email,
+  language,
+  interests,
+  comment
+}) => {
+  return (
+    <html>
+      <body>
+        The following message was submitted via the 2020 Census Get Involved Form:
+        <blockquote>
+          Name:
+          <p>
+            {firstName} {lastName}
+          </p>
+          Email:
+          <p>
+            <a href={`mailto:${email}`}>{email}</a>
+          </p>
+
+          Language:
+          <p>
+            {supportedLocaleEnglishNames[language] || language}
+          </p>
+
+          Visitor has an interest in:
+          <p>
+            <ul>
+              {interests.map(
+                (interest) => (
+                  <li key={interest}>
+                    {interest}
+                  </li>
+                )
+              )}
+            </ul>
+          </p>
+          {
+            comment ? (
+              <React.Fragment>
+                Additional comments:
+                <p>
+                  {comment}
+                </p>
+              </React.Fragment>
+            ) : null
+          }
+        </blockquote>
+
+        <p>
+          The visitor is expecting a response within two business days. The response was sent at {formatDate(new Date())}. Reply back to this email to respond to the visitor.
+        </p>
+      </body>
+    </html>
+  );
+};
+
+InquiryEmail.propTypes = {
+  language: PropTypes.oneOf(supportedLocales).isRequired,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  email: PropTypes.string.isRequired,
+  interests: PropTypes.arrayOf(PropTypes.string).isRequired,
+  comment: PropTypes.string
+};
