55896abe24c07f227c74c1fef3ab61d67e2209dc
Turner Hayes <dev@turnerhayes.com>
2020-01-28 21:45:28 -0800

    Revert unintended changes to components

--- client/components/LocalePicker.js
+++ client/components/LocalePicker.js
@@ -7,28 +7,28 @@ import {
 } from '../../i18n/supported-locales';
 import './LocalePicker.scss';
 
-import classnames from 'classnames';
+import classnames from 'classnames'
 
-import globe from '../images/globe.svg';
-import triangle from '../images/triangle.svg';
+import globe from '../images/globe.svg'
+import triangle from '../images/triangle.svg'
 
 export const LocalePicker = () => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
 
   return (
     <LocaleContext.Consumer>
       {
         ({ currentLocale, setLocale }) => (
           <div className={'c_locale-picker'}>
-            <img
+            <img 
               className="c_locale-picker__icon"
               src={globe}
               alt="Localization icon"
             />
             <div
               className={classnames(
-                'c_locale-picker__container',
-                'c_locale-picker__container--desktop'
+                "c_locale-picker__container",
+                "c_locale-picker__container--desktop"
               )}>
               {
                 supportedLocales.map(
@@ -37,8 +37,8 @@ export const LocalePicker = () => {
                       key={locale}
                       disabled={locale === currentLocale}
                       className={classnames(
-                        'c_locale-picker__container__option',
-                        'c_locale-picker__container__option--desktop'
+                        "c_locale-picker__container__option",
+                        "c_locale-picker__container__option--desktop"
                       )}
                       onClick={() => setLocale(locale)}
                     >
@@ -50,8 +50,8 @@ export const LocalePicker = () => {
             </div>
             <div
               className={classnames(
-                'c_locale-picker__container',
-                'c_locale-picker__container--mobile'
+                "c_locale-picker__container",
+                "c_locale-picker__container--mobile"
               )}>
               <button
                 disabled={true}
@@ -64,29 +64,29 @@ export const LocalePicker = () => {
                 <img
                   src={triangle}
                   className={classnames(
-                    'c_locale-picker__container__carrot__image',
-                    { 'c_locale-picker__container__carrot__image--open': open }
-                  )}
+                    "c_locale-picker__container__carrot__image",
+                    {"c_locale-picker__container__carrot__image--open": open}
+                  )} 
                 />
               </button>
               { open && (
                 <div
                   className="c_locale-picker__container__dropdown-container">
                   { supportedLocales.map(
-                    (locale) => (
-                      <button
-                        key={locale}
-                        disabled={locale === currentLocale}
-                        className={classnames(
-                          'c_locale-picker__container__option',
-                          { 'c_locale-picker__container__option--hide': (locale === currentLocale) }
-                        )}
-                        onClick={() => setLocale(locale)}
-                      >
-                        {supportedLocaleNames[locale]}
-                      </button>
+                      (locale) => (
+                        <button
+                          key={locale}
+                          disabled={locale === currentLocale}
+                          className={classnames(
+                            "c_locale-picker__container__option",
+                            {"c_locale-picker__container__option--hide": (locale === currentLocale )}
+                          )}
+                          onClick={() => setLocale(locale)}
+                        >
+                          {supportedLocaleNames[locale]}
+                        </button>
+                      )
                     )
-                  )
                   }
                 </div>
               )}
